a5309e8bb0b1313637b44ecdad477caa1be1f758
jahidanowar <milon5143@gmail.com>
2022-01-22 10:48:44 +0530

    1st DS Assignment

---
+++ DS/Stack.java
@@ -0,0 +1,70 @@
+package DS;
+
+public class Stack {
+    int top;
+    int[] stack;
+    int size;
+
+    public Stack(int size) {
+        this.size = size;
+        stack = new int[size];
+        top = -1;
+    }
+
+    public void push(int data) {
+        if (isFull()) {
+            System.out.println("Stack is full");
+            return;
+        }
+        stack[++top] = data;
+    }
+
+    public void pop() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty");
+            return;
+        }
+
+        top--;
+    }
+
+    public void printStack() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty");
+            return;
+        }
+        for (int i = top; i >= 0; i--) {
+            System.out.println(stack[i]);
+        }
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public boolean isFull() {
+        return top == size - 1;
+    }
+
+    public static void main(String[] args) {
+
+        Stack stack = new Stack(10);
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+        stack.push(40);
+        stack.pop();
+        stack.printStack();
+        stack.pop();
+        stack.printStack();
+        stack.push(33);
+        stack.push(44);
+        stack.printStack();
+        stack.pop();
+        stack.printStack();
+        stack.pop();
+        stack.printStack();
+    }
+
+}
