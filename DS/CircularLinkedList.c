faebcee0f7ce0037f538d0e4c5ee9bbf7473c515
jahidanowar <milon5143@gmail.com>
2022-02-24 12:04:17 +0530

    feat: Circular Linked list

--- DS/CircularLinkedList.c
+++ DS/CircularLinkedList.c
@@ -30,30 +30,118 @@ void append(struct Node **head, int data)
     *head = newNode;
 }
 
+/* push element to the list */
+void push(struct Node **head, int data)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = data;
+
+    if (*head == NULL)
+    {
+        newNode->next = newNode;
+        *head = newNode;
+        return;
+    }
+
+    newNode->next = (*head)->next;
+    (*head)->next = newNode;
+}
+
+/* Delete element from the circular list */
+void delete (struct Node **head, int data)
+{
+    struct Node *temp = *head;
+    struct Node *prev = NULL;
+
+    if (temp == NULL)
+    {
+        printf("List is empty\n");
+        return;
+    }
+
+    if (temp->data == data)
+    {
+        *head = temp->next;
+        free(temp);
+        return;
+    }
+
+    while (temp->data != data)
+    {
+        prev = temp;
+        temp = temp->next;
+        if (temp == *head)
+        {
+            printf("Element not found\n");
+            return;
+        }
+    }
+
+    prev->next = temp->next;
+    free(temp);
+}
+
 /* Display the linked list */
 void printLL(struct Node *head)
 {
 
-    struct Node *temp = *head;
+    struct Node *temp = head;
+    if (temp == NULL)
+    {
+        printf("List is empty\n");
+        return;
+    }
 
-    while (temp->next != head)
+    do
     {
         printf("%d ", temp->data);
         temp = temp->next;
-    }
+    } while (temp != head);
+    printf("\n");
 }
 
 int main()
 {
 
+    /* Menu Driven Program */
+    int choice;
     struct Node *head = NULL;
 
-    append(&head, 10);
-    append(&head, 12);
-    append(&head, 15);
-    append(&head, 20);
+    while (1)
+    {
+        printf("\n1. Append\n2. Push\n3. Delete\n4. Print\n5. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
 
-    printLL(head);
+        switch (choice)
+        {
+        case 1:
+            printf("Enter the element to be appended: ");
+            int data;
+            scanf("%d", &data);
+            append(&head, data);
+            break;
+        case 2:
+            printf("Enter the element to be pushed: ");
+            int data;
+            scanf("%d", &data);
+            push(&head, data);
+            break;
+        case 3:
+            printf("Enter the element to be deleted: ");
+            int data;
+            scanf("%d", &data);
+            delete (&head, data);
+            break;
+        case 4:
+            printLL(head);
+            break;
+        case 5:
+            exit(0);
+        default:
+            printf("Invalid choice\n");
+        }
+    }
 
     return 0;
 }
