12290430b175c7ed0e2c7f4b72f91ff1b16852fc
jahidanowar <milon5143@gmail.com>
2022-03-06 23:01:22 +0530

    quicksort

---
+++ DS/sorttest/sorttest/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  sorttest
+//
+//  Created by Jahid Anowar on 06/03/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
