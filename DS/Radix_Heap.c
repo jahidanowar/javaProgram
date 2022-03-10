581dfe52ad5cf3b19b6f385125ecbde992893494
jahidanowar <milon5143@gmail.com>
2022-03-10 11:26:34 +0530

    feat: Radix and Heap Sort

--- DS/Radix_Heap.c
+++ DS/Radix_Heap.c
@@ -1,5 +1,147 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// Getmax
+int getMax(int arr[], int n)
+{
+    int max = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+    return max;
+}
 
-int
+/* counting sort */
+
+void countSort(int arr[], int n, int exp)
+{
+    int output[n];
+    int i, count[10] = {0};
+
+    for (i = 0; i < n; i++)
+    {
+        count[(arr[i] / exp) % 10]++;
+    }
+
+    for (i = 1; i < 10; i++)
+    {
+        count[i] += count[i - 1];
+    }
+
+    for (i = n - 1; i >= 0; i--)
+    {
+        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+        count[(arr[i] / exp) % 10]--;
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        arr[i] = output[i];
+    }
+}
+
+void radixSort(int arr[], int n)
+{
+    int m = getMax(arr, n);
+
+    for (int exp = 1; m / exp > 0; exp *= 10)
+    {
+        countSort(arr, n, exp);
+    }
+}
+
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+void swap(int *a, int *b)
+{
+    int temp = *b;
+    *b = *a;
+    *a = temp;
+}
+
+void heapify(int arr[], int n, int i)
+{
+    int largest = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+
+    if (l < n && arr[l] > arr[largest])
+    {
+        largest = l;
+    }
+
+    if (r < n && arr[r] > arr[largest])
+    {
+        largest = r;
+    }
+
+    if (largest != i)
+    {
+        swap(&arr[i], &arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
+
+void heapSort(int arr[], int n)
+{
+    for (int i = n / 2 - 1; i >= 0; i--)
+    {
+        heapify(arr, n, i);
+    }
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        swap(&arr[0], &arr[i]);
+        heapify(arr, i, 0);
+    }
+}
+
+int main()
+{
+    /* Menu Driven Program */
+    int choice;
+    int n;
+    int *arr;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    arr = (int *)malloc(n * sizeof(int));
+
+    printf("Enter the elements: ");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("\n1. Radix Sort\n2. Heap Sort\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    switch (choice)
+    {
+    case 1:
+        radixSort(arr, n);
+        printArray(arr, n);
+        break;
+    case 2:
+        heapSort(arr, n);
+        printArray(arr, n);
+        break;
+    default:
+        printf("Invalid Choice\n");
+        break;
+    }
+
+    return 0;
+}
