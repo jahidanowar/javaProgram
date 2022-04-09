335b312eb2c9251850b3751d85c268d7cdf62021
jahidanowar <milon5143@gmail.com>
2022-04-09 22:55:41 +0530

    init

--- DS/GraphBFS.c
+++ DS/GraphBFS.c
@@ -3,136 +3,147 @@
  * @author Jahid A (2147033)
  *
  */
-#include <stdio.h>
-#include <stdlib.h>
+// BFS algorithm in C
 
-struct node
-{
-    int vertex;
-    struct node *next;
-};
-
-struct node *createNode(int v);
-
-struct Graph
-{
-    int numVertices;
-    int *visited;
-
-    // We need int** to store a two dimensional array.
-    // Similary, we need struct node** to store an array of Linked lists
-    struct node **adjLists;
-};
-
-// BFS algo
-void BFS(struct Graph *graph, int vertex)
-{
-    struct node *adjList = graph->adjLists[vertex];
-    struct node *temp = adjList;
-
-    graph->visited[vertex] = 1;
-    printf("Visited %d \n", vertex);
-
-    while (temp != NULL)
-    {
-        int connectedVertex = temp->vertex;
-
-        if (graph->visited[connectedVertex] == 0)
-        {
-            BFS(graph, connectedVertex);
-        }
-        temp = temp->next;
-    }
-}
-// Create a node
-struct node *createNode(int v)
-{
-    struct node *newNode = malloc(sizeof(struct node));
-    newNode->vertex = v;
-    newNode->next = NULL;
-    return newNode;
-}
-
-// Create graph
-struct Graph *createGraph(int vertices)
-{
-    struct Graph *graph = malloc(sizeof(struct Graph));
-    graph->numVertices = vertices;
-
-    graph->visited = malloc(vertices * sizeof(int));
-    graph->adjLists = malloc(vertices * sizeof(struct node *));
-
-    int i;
-    for (i = 0; i < vertices; i++)
-    {
-        graph->adjLists[i] = NULL;
-        graph->visited[i] = 0;
-    }
-    return graph;
-}
-
-// Add edge
-void addEdge(struct Graph *graph, int src, int dest)
-{
-    // Add edge from src to dest
-    struct node *newNode = createNode(dest);
-    newNode->next = graph->adjLists[src];
-    graph->adjLists[src] = newNode;
-
-    // Add edge from dest to src
-    newNode = createNode(src);
-    newNode->next = graph->adjLists[dest];
-    graph->adjLists[dest] = newNode;
-}
-
-// Print the graph
-void printGraph(struct Graph *graph)
-{
-    int v;
-    for (v = 0; v < graph->numVertices; v++)
-    {
-        struct node *temp = graph->adjLists[v];
-        printf("\n Adjacency list of vertex %d\n ", v);
-        while (temp)
-        {
-            printf("%d -> ", temp->vertex);
-            temp = temp->next;
-        }
-        printf("\n");
-    }
-}
-
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX 100  
+ 
+#define initial 1
+#define waiting 2
+#define visited 3
+ 
+int n;    
+int adj[MAX][MAX];
+int state[MAX];
+void create_graph();
+void BF_Traversal();
+void BFS(int v);
+ 
+int queue[MAX], front = -1,rear = -1;
+void insert_queue(int vertex);
+int delete_queue();
+int isEmpty_queue();
+ 
 int main()
 {
-    // Menu Driven Program
-    int choice;
-    int n;
-    int *arr;
-
-    printf("Enter the number of elements: ");
-
-    scanf("%d", &n);
-
-    arr = (int *)malloc(n * sizeof(int));
-
-    struct Graph *graph = createGraph(n);
-
-    int i;
-    for (i = 0; i < n; i++)
-    {
-        printf("Enter vertex %d: ", i);
-        scanf("%d", &arr[i]);
-    }
-
-    for (i = 0; i < n; i++)
-    {
-        addEdge(graph, arr[i], i);
-    }
-
-    printGraph(graph);
-
-    printf("\nDFS traversal: ");
-    BFS(graph, 0);
-
+    create_graph();
+    BF_Traversal();
     return 0;
 }
+ 
+void BF_Traversal()
+{
+int v;
+for(v=0; v<n; v++)
+state[v] = initial;
+printf("Enter Start Vertex for BFS: \n");
+scanf("%d", &v);
+BFS(v);
+}
+ 
+void BFS(int v)
+{
+int i;
+insert_queue(v);
+state[v] = waiting;
+while(!isEmpty_queue())
+{
+v = delete_queue( );
+printf("%d ",v);
+state[v] = visited;
+for(i=0; i<n; i++)
+{
+if(adj[v][i] == 1 && state[i] == initial)
+{
+insert_queue(i);
+state[i] = waiting;
+}
+}
+}
+printf("\n");
+}
+ 
+void insert_queue(int vertex)
+{
+if(rear == MAX-1)
+printf("Queue Overflow\n");
+else
+{
+if(front == -1)
+front = 0;
+rear = rear+1;
+queue[rear] = vertex ;
+}
+}
+ 
+int isEmpty_queue()
+{
+if(front == -1 || front > rear)
+return 1;
+else
+return 0;
+}
+ 
+int delete_queue()
+{
+int delete_item;
+if(front == -1 || front > rear)
+{
+printf("Queue Underflow\n");
+exit(1);
+}
+delete_item = queue[front];
+front = front+1;
+return delete_item;
+}
+ 
+void create_graph()
+{
+int count,max_edge,origin,destin;
+ 
+printf("Enter number of vertices : ");
+scanf("%d",&n);
+max_edge = n*(n-1);
+ 
+for(count=1; count<=max_edge; count++)
+{
+printf("Enter edge %d( -1 -1 to quit ) : ",count);
+scanf("%d %d",&origin,&destin);
+ 
+if((origin == -1) && (destin == -1))
+break;
+ 
+if(origin>=n || destin>=n || origin<0 || destin<0)
+{
+printf("Invalid edge!\n");
+count--;
+}
+else
+{
+adj[origin][destin] = 1;
+}
+}
+}
+
+/*
+  addEdge(graph, 0, 1);
+  addEdge(graph, 0, 2);
+  addEdge(graph, 1, 2);
+  addEdge(graph, 1, 4);
+  addEdge(graph, 1, 3);
+  addEdge(graph, 2, 4);
+  addEdge(graph, 3, 4);
+*/
+
+/* 
+Enter number of vertices : 4
+Enter edge 1( -1 -1 to quit ) : 0 1
+Enter edge 2( -1 -1 to quit ) : 0 2
+Enter edge 3( -1 -1 to quit ) : 1 2
+Enter edge 4( -1 -1 to quit ) : 2 0
+Enter edge 5( -1 -1 to quit ) : 2 3
+Enter edge 6( -1 -1 to quit ) : 3 3
+Enter edge 7( -1 -1 to quit ) : -1 -1
+Enter Start Vertex for BFS:  2
+*/
